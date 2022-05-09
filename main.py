d26367ddcd8bd831ec5324cce81f10696c0e7c91
tyfvf <tyfvf2@gmail.com>
2022-05-09 18:26:51 -0300

    Started Minesweeper in Python, divided the screen placements

---
+++ main.py
@@ -0,0 +1,37 @@
+from tkinter import *
+import settings
+import utils
+
+root = Tk()
+#  Override the setting of the window
+root.configure(bg='#000')
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
+root.title('Minesweeper Game')
+root.resizable(False, False)
+
+top_frame = Frame(
+    root,
+    bg='red',
+    width=settings.WIDTH,
+    height=utils.height_prct(25)
+)
+top_frame.place(x=0, y=0)
+
+left_frame = Frame(
+    root,
+    bg='blue',
+    width=utils.width_prct(25),
+    height=utils.height_prct(75)
+)
+left_frame.place(x=0, y=utils.height_prct(25))
+
+center_frame = Frame(
+    root,
+    bg='green',
+    width=utils.width_prct(75),
+    height=utils.height_prct(75)
+)
+center_frame.place(x=utils.width_prct(25), y=utils.height_prct(25))
+
+#  Run the window
+root.mainloop()
