50f6a0acba7ab32ec719f0ed834f19c4c59ce83c
tyfvf <tyfvf2@gmail.com>
2022-05-10 13:49:59 -0300

    Added the logic behind making some cells turn into bombs

--- cell.py
+++ cell.py
@@ -1,15 +1,25 @@
 from tkinter import Button
+import random
 
 
 class Cell:
-    def __init__(self, is_mine=False):
+    all = []
+
+    def __init__(self,x,y, is_mine=False):
         self.is_mine = is_mine
         self.cell_btn_object = None
+        self.x = x
+        self.y = y
+
+        # Append the object to the Cell.all list
+        Cell.all.append(self)
 
     def create_btn_object(self, location):
         btn = Button(
             location,
-            text='Text'
+            width=12,
+            height=4,
+            text=f'{self.x},{self.y}'
         )
         btn.bind('<Button-1>', self.left_click_actions)  # Left Click
         btn.bind('<Button-3>', self.right_click_actions)  # Right Click
@@ -22,3 +32,12 @@ class Cell:
     def right_click_actions(self, event):
         print(event)
         print('I am right clicked!')
+
+    @staticmethod
+    def randomize_mines():
+        picked_cells = random.sample(Cell.all, 9)
+        for picked_cell in picked_cells:
+            picked_cell.is_mine = True
+
+    def __repr__(self):
+        return f'Cell({self.x}, {self.y})'
--- main.py
+++ main.py
@@ -36,12 +36,16 @@ center_frame.place(x=utils.width_prct(25), y=utils.height_prct(25))
 
 for x in range(settings.GRID_SIZE):
     for y in range(settings.GRID_SIZE):
-        c = Cell()
+        c = Cell(x, y)
         c.create_btn_object(center_frame)
         c.cell_btn_object.grid(
             column=x,
             row=y
         )
+
+Cell.randomize_mines()
+for c in Cell.all:
+    print(c.is_mine)
 
 #  Run the window
 root.mainloop()
